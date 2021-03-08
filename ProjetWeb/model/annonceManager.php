0a1f78237d35839c781c1f56b722265ba05aa7cd
Shanshe.GUNDISHVILI <Shanshe.GUNDISHVILI@cpnv.ch>
2021-03-08 10:33:33 +0100

    added ID mechanisme

--- ProjetWeb/model/annonceManager.php
+++ ProjetWeb/model/annonceManager.php
@@ -2,5 +2,17 @@
 
 function annonceToJson($data)
 {
+
+    $arr[] =json_decode(file_get_contents("data/annonce.json"),true);
+    $last = end($arr);
+    $lastID = $last['ID'] +1;
+
+
+    $data['Email']= $_SESSION['userEmailAddress'];
+    $data['active'] = true;
+    $data['ID']= $lastID;
+
     file_put_contents("data/annonce.json", json_encode($data));
+
 }
+
