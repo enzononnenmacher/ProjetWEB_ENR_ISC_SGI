3a3fa94dd24c95bf871b469d9104d295315a5da0
Shanshe.GUNDISHVILI <Shanshe.GUNDISHVILI@cpnv.ch>
2021-03-18 15:02:51 +0100

    implemented better json Encode 18.03.2021 last version

--- ProjetWeb/model/annonceManager.php
+++ ProjetWeb/model/annonceManager.php
@@ -2,13 +2,16 @@
 
 function annonceToJson($data)
 {
-   $arr = json_decode(file_get_contents("data/annonce.json"),true);
 
-    $count = 0;
-    foreach ($arr as $rand){
-        $count++;
+    $arr['eee'] ="aweqw";
+    $arr = json_decode(file_get_contents("data/annonce.json"),true);
+
+    if($arr) {
+        $count=0;
+        foreach ($arr as $rand) {
+            $count++;
+        }
     }
-
 
 
 
