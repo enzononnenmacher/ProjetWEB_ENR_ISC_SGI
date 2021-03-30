315754dc9dc80b0824ddd471d4c1b246364f507d
ShansheGundishvili <61775646+ShansheGundishvili@users.noreply.github.com>
2021-03-30 08:49:26 +0200

    implemented image upload

--- ProjetWeb/model/annonceManager.php
+++ ProjetWeb/model/annonceManager.php
@@ -20,13 +20,50 @@ function annonceToJson($data)
     $new['ID']= $count;
 
 
+
+
+
+    $file = $_FILES['inputPictures'];
+
+    $fileName = $_FILES['inputPictures']['name'];
+    $fileTmpName = $_FILES['inputPictures']['tmp_name'];
+    $fileSize = $_FILES['inputPictures']['size'];
+    $fileError = $_FILES['inputPictures']['error'];
+
+
+    $tmpFileExt = explode('.', $fileName);
+    $fileExt = strtolower(end($tmpFileExt));
+
+    $allowed = array('jpeg', 'jpg' , 'png');
+
+
     $data['Email'] =$new['Email'];
     $data['active'] =$new['active'];
     $data['ID'] =$new['ID'];
+    $fileNameNew = $data['Email']."-".$data['ID'].".".$fileExt;
+    $data['inputPictures'] = "data/images/".$fileNameNew;
+
+
     array_push($arr, $data);
 
 
-    file_put_contents("data/annonce.json", json_encode($arr));
+    if(in_array($fileExt, $allowed)){
+        if($fileError === 0){
+            if($fileSize < 1000000){
+                $fileNameNew = $data['Email']."-".$data['ID'].".".$fileExt;
+                $fileDestination = "data/images/".$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                file_put_contents("data/annonce.json", json_encode($arr));
+            }else{
+
+                echo "le fichier est trop grand!";
+            }
+        }else{
+            echo "il y a eu une erreur lors du chargement!";
+        }
+    }else{
+        echo "vous ne pouvez pas uploader des fichiers de ce type!";
+    }
 }
 
 
--- ProjetWeb/view/all.php
+++ ProjetWeb/view/all.php
@@ -29,8 +29,8 @@ ob_start();
                         <?php foreach($articles as $article) :?>
                             <div class="s-12 m-12 l-4 margin-m-bottom div-row">
                                 <a>
-                                    <img src="view/img/appartement/1.jpg" alt="">
-                                    <a href="index.php?action=adDetails&ID=<?= $article['ID']; ?>"><br><strong><?=$article['inputNameAnnonce']; ?></strong><br></a>
+                                    <img src=<?=$article['inputPictures']; ?>>
+                                    <a href="../index.php?action=adDetails&ID=<?= $article['ID']; ?>"><br><strong><?=$article['inputNameAnnonce']; ?></strong><br></a>
                                     <?=$article['inputPrice']; ?> CHF<br>
                                     <?=$article['inputCity']; ?><br><br>
                                 </a>
